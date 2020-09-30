a20e33068dcc6fff95a396576d905add60d687e7
c0dexter <michal.dobrowolski1@gmail.com>
2020-09-30 22:36:10 +0200

    Added DriverManager class

---
+++ src/test/java/driver/manager/DriverManager.java
@@ -0,0 +1,27 @@
+package driver.manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+    private static WebDriver driver;
+
+    private DriverManager() {
+    }
+
+    public static WebDriver getWebDriver() {
+
+        if (driver == null) {
+            System.setProperty("webdriver.chrome.driver", "drivers/repository/chrome/chromedriver");
+            driver = new ChromeDriver();
+        }
+
+        return driver;
+    }
+
+    public static void disposeDriver(){
+        driver.close();
+        driver.quit();
+        driver = null;
+    }
+}
