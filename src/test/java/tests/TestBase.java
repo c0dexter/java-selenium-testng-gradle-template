eba900e0882df7579d7756d3f3cf9dc3a644d481
c0dexter <michal.dobrowolski1@gmail.com>
2020-10-06 00:10:39 +0200

    Added TestBase class with methods @BeforeMethod and @AfterMethod

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,2 +1,25 @@
-package tests;public class TestBase {
+package tests;
+
+import driver.manager.DriverManager;
+import driver.manager.DriverUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import page.objects.BasePage;
+
+public class TestBase extends BasePage {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() {
+        driver = DriverManager.getWebDriver();
+        DriverUtils.setInitialConfiguration();
+        DriverUtils.navigateToPage("http://automationpractice.com/index.php");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        DriverManager.disposeDriver();
+    }
 }
