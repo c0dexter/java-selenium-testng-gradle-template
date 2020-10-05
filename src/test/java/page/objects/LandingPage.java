88cadad86f11d573b3905fcffc2ef9821b60e1c5
c0dexter <michal.dobrowolski1@gmail.com>
2020-10-06 00:09:05 +0200

    Added the LangingPage page object by using the FluentInterface approach

--- src/test/java/page/objects/LandingPage.java
+++ src/test/java/page/objects/LandingPage.java
@@ -1,2 +1,18 @@
-package page.objects;public class LandingPage {
+package page.objects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import waits.WaitForElement;
+
+public class LandingPage extends BasePage {
+
+    @FindBy(id = "header_logo")
+    WebElement headerLogo;
+
+    public LandingPage clickOnHeaderLogo() {
+        WaitForElement.waitUntilElementIsClickable(headerLogo);
+        headerLogo.click();
+        log().info("Clicked on the header logo");
+        return new LandingPage();
+    }
 }
